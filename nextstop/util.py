c34dcc1408009f515c1f6f22c454fea68288a7eb
RollermineCoaster <50976062+RollermineCoaster@users.noreply.github.com>
2023-03-27 02:03:55 +0800

    Visual and performance improvement
    - Added custom logo
    - Changed the structure of the Simple mode to improve performance
    - Changed the text resize logic on Fancy mode

--- nextstop/util.py
+++ nextstop/util.py
@@ -1,3 +1,24 @@
 import math
+
 def getDistance(pos1, pos2):
     return math.sqrt((pos1[0]-pos2[0])**2 + (pos1[1]-pos2[1])**2 + (pos1[2]-pos2[2])**2)
+
+def getCanvasObjHeight(canvas, id):
+    _, y1, _, y2 = canvas.bbox(id)
+    return abs(y2-y1)
+
+def resizeCanvasText(canvas, id, width):
+    #remove the width limit of the object
+    canvas.itemconfigure(id, width=0)
+    if type(width) == "str":
+        width = canvas.winfo_fpixels(width)
+    #make system name resize dynamically
+    textHeight = getCanvasObjHeight(canvas, id)
+    textFont, textSize = canvas.itemcget(id, "font").split()
+    textSize = int(textSize)
+    #limit the width of the object
+    canvas.itemconfigure(id, width=width)
+    #while text size > 1pt and current text height > old text height
+    while textSize > 1 and getCanvasObjHeight(canvas, id) > textHeight:
+        textSize -= 1
+        canvas.itemconfigure(id, font=(textFont, textSize))
