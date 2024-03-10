e1f501601aec11c062562fde87645e5d18dd0e54
RollermineCoaster <50976062+RollermineCoaster@users.noreply.github.com>
2024-03-11 00:15:50 +0800

    Update ui.py
    - Change canvas size unit from pixel to point
    - Change color variables to separate variables
    - Added some comment

--- nextstop/ui.py
+++ nextstop/ui.py
@@ -27,6 +27,12 @@ SCOOPABLE = ["O","B","A","F","G","K","M"]
 #white dwarfs, neutron stars, black holes
 DANGER = ["D","N","H"]
 
+#color
+DANGERCOLOR = "#F00"
+THEME1933 = {"bg": "#FFF", "textMain": "#000", "textMinor": "#555", "main": "#F00", "minor1": "#888", "minor2": "#BBB"}
+THARGOIDCOLORS = {"Alert": "#FFD00F", "Invasion": "#FF6F00", "Controlled": "#286300", "Titan": "#900", "Recovery": "#9F1BFF"}
+
+#font
 LOGOFONT = "nextstop-logo"
 FUELSTARLOGO =    "\uE800"
 DANGERLOGO =      "\uE801"
@@ -35,8 +41,7 @@ EDSMLOGO =        "\uE820"
 BULLETBG =        "\uF111"
 BULLETFG =        "\uF10C"
 
-THARGOIDCOLORS = {"Alert": "#FFD00F", "Invasion": "#FF6F00", "Controlled": "#286300", "Maelstrom": "#900", "Recovery": "#9F1BFF"}
-
+#string
 CURRENT = "CURRENT"
 
 class BaseBoard(ABC):
@@ -46,7 +51,7 @@ class BaseBoard(ABC):
         self.thargoidSystems= {}
         self.currentIndex = 0
         self.currentPos = [0.0, 0.0, 0.0]
-        self.size = 300*config.get_int("ui_scale")/100
+        self.size = frame.winfo_fpixels("225p")
         #create canvas
         self.canvas = tk.Canvas(frame, width=self.size, height=0, bd=0, highlightthickness=0)
         self.canvas.grid()
@@ -153,7 +158,6 @@ class SimpleBoard(BaseBoard):
 
     def __init__(self, frame):
         super().__init__(frame)
-        self.colors = {"danger": "#f00"}
         self.styles = {}
         self.styles["system"] =       {"x": 0,               "option": {"anchor": tk.NW, "justify": tk.LEFT}}
         self.styles["starType"] =     {"x": 0,               "option": {"anchor": tk.NW, "justify": tk.LEFT}}
@@ -242,19 +246,21 @@ class FancyBoard(BaseBoard):
 
     def __init__(self, frame):
         super().__init__(frame)
-        self.colors = {"bg": "#fff", "textMain": "#000", "textMinor": "#555", "main": "#f00", "minor1": "#888", "minor2": "#bbb", "danger": "#f00"}
+        self.colors = THEME1933
         self.rowHeight = self.size/6
         self.styles = {}
-        self.styles["bulletBG"] =     {"x": self.rowHeight/2, "y": self.rowHeight/2,  "option": {"anchor": tk.CENTER, "fill": self.colors["minor2"],    "font": (LOGOFONT,    12), "justify": tk.CENTER, "text":BULLETBG}}
-        self.styles["bulletFG"] =     {"x": self.rowHeight/2, "y": self.rowHeight/2,  "option": {"anchor": tk.CENTER, "fill": self.colors["minor1"],    "font": (LOGOFONT,    12), "justify": tk.CENTER, "text":BULLETFG}}
-        self.styles["system"] =       {"x": self.rowHeight,   "y": self.rowHeight*.3, "option": {"anchor": tk.W,      "fill": self.colors["textMain"],  "font": ('Helvetica', 12), "justify": tk.LEFT}}
-        self.styles["starType"] =     {"x": self.rowHeight,   "y": self.rowHeight*.7, "option": {"anchor": tk.W,      "fill": self.colors["textMinor"], "font": ('Helvetica', 9),  "justify": tk.LEFT}}
-        self.styles["distance"] =     {"x": self.size*.95,    "y": self.rowHeight*.3, "option": {"anchor": tk.E,      "fill": self.colors["textMinor"], "font": ('Helvetica', 11), "justify": tk.RIGHT}}
-        self.styles["reminder"] =     {"x": self.size*.95,    "y": self.rowHeight*.7, "option": {"anchor": tk.E,      "fill": self.colors["textMinor"], "font": (LOGOFONT,    20), "justify": tk.RIGHT}}
-        self.styles["edsmLogo"] =     {"x": self.size*.86,    "y": self.rowHeight*.7, "option": {"anchor": tk.E,      "fill": self.colors["textMinor"], "font": (LOGOFONT,    20), "justify": tk.RIGHT}}
-        self.styles["thargoidLogo"] = {"x": self.size*.77,    "y": self.rowHeight*.7, "option": {"anchor": tk.E,      "fill": self.colors["textMinor"], "font": (LOGOFONT,    20), "justify": tk.RIGHT}}
-        self.styles["bottomLine"] = {"x0": self.size*.025,   "x1": self.size*.975,   "y0": self.rowHeight,   "y1": self.rowHeight,   "option": {"fill": self.colors["minor1"], "width": "0.766p"}}
-        self.styles["bulletLine"] = {"x0": self.rowHeight/2, "x1": self.rowHeight/2, "y0": self.rowHeight/2, "y1": self.rowHeight/2, "option": {"fill": self.colors["main"],   "width": "1.5p"}}
+        #text style
+        self.styles["bulletBG"] =     {"type": "text", "x": self.rowHeight/2, "y": self.rowHeight/2,  "option": {"anchor": tk.CENTER, "fill": self.colors["minor2"],    "font": (LOGOFONT,    12), "text":BULLETBG}}
+        self.styles["bulletFG"] =     {"type": "text", "x": self.rowHeight/2, "y": self.rowHeight/2,  "option": {"anchor": tk.CENTER, "fill": self.colors["minor1"],    "font": (LOGOFONT,    12), "text":BULLETFG}}
+        self.styles["system"] =       {"type": "text", "x": self.rowHeight,   "y": self.rowHeight*.3, "option": {"anchor": tk.W,      "fill": self.colors["textMain"],  "font": ('Helvetica', 12)}}
+        self.styles["starType"] =     {"type": "text", "x": self.rowHeight,   "y": self.rowHeight*.7, "option": {"anchor": tk.W,      "fill": self.colors["textMinor"], "font": ('Helvetica', 9)}}
+        self.styles["distance"] =     {"type": "text", "x": self.size*.95,    "y": self.rowHeight*.3, "option": {"anchor": tk.E,      "fill": self.colors["textMinor"], "font": ('Helvetica', 11)}}
+        self.styles["reminder"] =     {"type": "text", "x": self.size*.95,    "y": self.rowHeight*.7, "option": {"anchor": tk.E,      "fill": self.colors["textMinor"], "font": (LOGOFONT,    20)}}
+        self.styles["edsmLogo"] =     {"type": "text", "x": self.size*.86,    "y": self.rowHeight*.7, "option": {"anchor": tk.E,      "fill": self.colors["textMinor"], "font": (LOGOFONT,    20)}}
+        self.styles["thargoidLogo"] = {"type": "text", "x": self.size*.77,    "y": self.rowHeight*.7, "option": {"anchor": tk.E,      "fill": self.colors["textMinor"], "font": (LOGOFONT,    20)}}
+        #line style
+        self.styles["bottomLine"] = {"type": "line", "x0": self.size*.025,   "x1": self.size*.975,   "y0": self.rowHeight,   "y1": self.rowHeight,   "option": {"fill": self.colors["minor1"], "width": "0.766p"}}
+        self.styles["bulletLine"] = {"type": "line", "x0": self.rowHeight/2, "x1": self.rowHeight/2, "y0": self.rowHeight/2, "y1": self.rowHeight/2, "option": {"fill": self.colors["main"],   "width": "1.5p"}}
         self.rowObjs = []
         self.canvas.config(bg=self.colors["bg"])
         self.updateCanvas()
@@ -299,7 +305,7 @@ class FancyBoard(BaseBoard):
                 #setup reminder logo
                 self.canvas.itemconfigure(rowObj["reminder"], text=self.getReminderText(index))
                 if self.getReminderText(index) == DANGERLOGO:
-                    self.canvas.itemconfigure(rowObj["reminder"], fill=self.colors["danger"])
+                    self.canvas.itemconfigure(rowObj["reminder"], fill=DANGERCOLOR)
                 #setup edsm logo
                 if self.getEDSMUrl(index) == "":
                     self.canvas.itemconfigure(rowObj["edsmLogo"], text="")
@@ -321,3 +327,6 @@ class FancyBoard(BaseBoard):
                     self.canvas.create_line(self.styles["bottomLine"]["x0"], self.styles["bottomLine"]["y0"]+self.rowHeight*(index), self.styles["bottomLine"]["x1"], self.styles["bottomLine"]["y1"]+self.rowHeight*(index), **self.styles["bottomLine"]["option"])
         totalRow = max(len(self.route), 1)
         self.resizeCanvas((0,0 ,self.size, self.rowHeight*totalRow))
+
+    def updateTheme(self):
+        super().updateTheme()
